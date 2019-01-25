01f119f5d027bb550cbe1f7d4eaaaa5ccfd08273
shobhitsingh29 <shobhitsingh29@gmail.com>
2019-01-25 16:40:12 +0530

    running woith sass --hot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import style from "./search.scss"
 
 const Index = () => {
-  return <div>Hello React!</div>;
+  return <div className={style.nameOfYourCSSClass}>Hello React !</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,33 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: "[name]_[local]_[hash:base64]",
+            sourceMap: true,
+            minimize: true
+          },
+        }, 'sass-loader'],
+        include: [/src\/(.*\/)?@(.*)?\/.*/]
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/src\/styles/, /src\/(.*\/)?@(.*)?\/.*/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
+
   },
   plugins: [
     new HtmlWebPackPlugin({
@@ -23,4 +48,4 @@ module.exports = {
       filename: "./index.html"
     })
 ]
-}
+};
